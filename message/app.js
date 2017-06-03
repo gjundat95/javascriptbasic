a16875f098cf45c1df37938f9aea2bf3748cbe51
gjundat95 <gjundat95@gmail.com>
2017-06-03 08:47:42 +0700

    update code 1

--- message/app.js
+++ message/app.js
@@ -1,3 +1,26 @@
 /**
  * Created by tinhngo on 6/3/2017.
  */
+var alertTest = function () {
+    alert('alo moto...');
+    alert(1);
+    alert(true);
+}
+
+var confirmTest = function () {
+    var result = confirm('are you delete item ?');
+    if(result == true){
+        alert('user accepted');
+    }else {
+        alert('user not accepted');
+    }
+}
+
+var promptTest = function () {
+    var result = prompt('enter username: ','tinhngo');
+    if(result != null){
+        alert("My name is "+result)
+    }
+}
+
+promptTest()
